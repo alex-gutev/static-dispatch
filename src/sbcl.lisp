66c43ddf8b8ffd0846a74dd1b8b3205731be2239
Alexander Gutev <alex.gutev@gmail.com>
2021-04-16 10:45:24 +0200

    Change SBCL IR1 transforms to be added only when ENABLE-STATIC-DISPATCH is called.

--- src/sbcl.lisp
+++ src/sbcl.lisp
@@ -30,33 +30,104 @@
 
 ;;; Compiler Transform
 
-(defun set-method-compiler-macro (name qualifier specializers lambda-list body)
-  (declare (ignore qualifier lambda-list body))
-  (with-gensyms (args)
-    `(progn
-       ;; Make function known to SBCL. Wrap in handler-case to handle
-       ;; error when function is already known
-       (handler-case (sb-c:defknown ,name * *)
-	 (error ()))
+(defmacro enable-static-dispatch (&rest names)
+  "Enable static dispatching for generic functions with names NAMES."
 
-       (sb-c:deftransform ,name ((&rest ,args) (,@specializers &rest *) * :policy (> speed safety))
-	 ;; TODO: The :NODE argument can be used to obtain the
-	 ;; compilation node from which the lexical environment of the
-	 ;; function call can be obtained using (SB-C::NODE-LEXENV
-	 ;; NODE), which can be used to base the inlining logic on
-	 ;; whether there is an inline declaration, in force.
-	 ;;
-	 ;; Unfortunately the SBCL node functions are not exported
-	 ;; from the package and are highly likely to be changed in
-	 ;; the future which will break not only this library but all
-	 ;; code which uses it.
+  `(progn ,@(mapcar (curry #'list 'enable-static-dispatch%) names)))
 
-	 (or (static-overload ',name ',args ',specializers)
-	     (sb-c::give-up-ir1-transform))))))
+(defmacro enable-static-dispatch% (name)
+  (labels ((make-transforms (method prior)
+	     ;; Generate the DEFTRANSFORM forms for a given METHOD and
+	     ;; given list of the PRIOR method specializers.
 
-(defun set-defgeneric-compiler-macro (name methods)
-  (mapcar (curry #'apply #'set-method-compiler-macro name) methods))
+	     (with-slots (specializers) method
+	       (let ((types (make-types specializers prior)))
+		 (mapcar #'make-transform types))))
 
+	   (make-transform (types)
+	     ;; Generate the DEFTRANSFORM form for a given list of
+	     ;; TYPES
+
+	     (with-gensyms (args)
+	       `(sb-c:deftransform ,name ((&rest ,args) (,@types &rest *) * :policy (> speed safety))
+		  (or (static-overload ',name ',args ',types)
+		      (sb-c::give-up-ir1-transform)))))
+
+	   (make-types (types prior)
+	     ;; For each T type in TYPES, return a specializer list
+	     ;; with the T replaced with the negation of the
+	     ;; corresponding type in each PRIOR specializer list, for
+	     ;; which the prefix of specializer list matches TYPES.
+
+	     (let ((ts (positions t types)))
+	       (if ts
+		   (mapcar (rcurry #'make-negated-types types prior) ts)
+		   (list types))))
+
+	   (make-negated-types (pos types prior)
+	     ;; Substitute the type at index POS with the negation of
+	     ;; all the corresponding types of all PRIOR specializer
+	     ;; lists which have the same prefix as TYPES.
+
+	     (let ((prior (filter-prior pos types prior))
+		   (types (copy-seq types)))
+
+	       (if prior
+		   (aprog1 (copy-seq types)
+		     (setf (elt it pos) `(not (or ,@prior))))
+		   types)))
+
+	   (filter-prior (pos types prior)
+	     ;; Remove prior specializer lists from PRIOR which do not
+	     ;; have the same prefix as TYPES in the range [0, POS).
+
+	     (let ((prefix (subseq types 0 pos)))
+	       (->> (remove-if-not (curry #'is-prefix prefix) prior)
+		    (mapcar (rcurry #'elt pos))
+		    (remove-duplicates))))
+
+	   (is-prefix (prefix types)
+	     ;; Check if a specializer list TYPES matches a given
+	     ;; PREFIX.
+
+	     (starts-with-subseq prefix types :test #'type-match))
+
+	   (type-match (a b)
+	     ;; Check that two types match
+
+	     (match* (a b)
+	       ((t _) t)
+	       ((_ t) t)
+	       ((_ _) (equal a b))))
+
+	   (sort-methods (methods)
+	     (sort methods #'method< :key #'car))
+
+	   (method< (m1 m2)
+	     (ematch* (m1 m2)
+	       (((list _ s1)
+		 (list _ s2))
+
+		(specializer< s1 s2)))))
+
+    (let* ((gf (fdefinition name))
+	   (precedence (precedence-order (generic-function-lambda-list gf) (generic-function-argument-precedence-order gf)))
+	   (methods (-<> (aand (gf-methods name) (hash-table-alist it))
+			 (order-method-specializers precedence)
+			 (sort-methods)
+			 (remove-duplicates :key #'cadar :test #'equal)
+			 (mapcar #'cdr <>))))
+      (unless methods
+	(warn "No methods found for generic function ~a" name))
+
+      `(progn
+	 (sb-c:defknown ,name * * () :overwrite-fndb-silently t)
+
+	 ,@ (reverse
+	     (loop for method in methods
+		for transform = (make-transforms method prior)
+		collect (specializers method) into prior
+		append transform))))))
 
 (defun static-dispatch (whole &optional env)
   "A no-op on SBCL since static dispatching is handled by the compiler
@@ -81,3 +152,15 @@
 	  `(progn
 	     (static-dispatch-test-hook)
 	     ,(inline-methods methods args nil types)))))))
+
+
+;;; Utilities
+
+(defun positions (item seq)
+  "Return a list containing the positions of the elements of sequence
+   SEQ which are EQL to ITEM."
+
+  (loop
+     for i from 0
+     for elem in seq
+     when (eql item elem) collect i))
